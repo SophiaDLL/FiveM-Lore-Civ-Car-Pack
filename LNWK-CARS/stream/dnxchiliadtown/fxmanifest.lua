1315dd114e4f042677a61f1ae72cb2dcd52d461f
Sophia <sophia@zwrks.com>
2025-01-30 06:31:39 +0000

    .

---
+++ LNWK-CARS/stream/dnxchiliadtown/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+
+authors { 'DANIX' }
+description 'DNX Chiliad Town'
+version '0.2 Early Access'
+ url 'https://www.nexusmods.com/gta5/mods/803'
+
+this_is_a_map 'yes'
+
+data_file "DLC_ITYP_REQUEST" "dnxchiliadtown_buildings.ytyp"
+data_file "DLC_ITYP_REQUEST" "dnxchiliadtown_land.ytyp"
+data_file "DLC_ITYP_REQUEST" "dnxchiliadtown_props.ytyp"
+data_file "DLC_ITYP_REQUEST" "dnxchiliadtown_road01.ytyp"
+data_file "DLC_ITYP_REQUEST" "dnxchiliadtown_int_underpass01.ytyp"
+data_file "DLC_ITYP_REQUEST" "dnxchiliadtown_int_plazabuilding.ytyp"
+data_file "DLC_ITYP_REQUEST" "dnxchiliadtown_int_247.ytyp"
+data_file "DLC_ITYP_REQUEST" "dnxchiliadtown_int_sheriffoffice.ytyp"
